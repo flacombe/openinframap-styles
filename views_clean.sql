a7f2d26e448d1b3517c0f90b9bafe629a93f3461
FLacombe <francois.lacombe@infos-reseaux.com>
2021-02-24 22:59:46 +0100

    Fix matviews refresh

--- views_clean.sql
+++ views_clean.sql
@@ -7,4 +7,7 @@ DROP VIEW IF EXISTS power_plant;
 DROP MATERIALIZED VIEW IF EXISTS power_plant_relation;
 
 /* Projects enedis */
+DROP VIEW pdm_boundary;
+DROP VIEW pdm_boundary_subdivide;
 DROP MATERIALIZED VIEW pdm_project_poteaux;
+DROP MATERIALIZED VIEW pdm_project_substations;
